119182e885251d27e29ebd3deb80259a2c56ab15
alirezanaseri548 <alirezanaseri369@gmail.com>
2026-07-29 13:53:32 +0330

    feat: add binary search implementation

---
+++ src/algorithms/binary-search.ts
@@ -0,0 +1,21 @@
+export function binarySearch(items: number[], target: number): number {
+  let left = 0;
+  let right = items.length - 1;
+
+  while (left <= right) {
+    const middle = Math.floor((left + right) / 2);
+    const value = items[middle];
+
+    if (value === target) {
+      return middle;
+    }
+
+    if (value < target) {
+      left = middle + 1;
+    } else {
+      right = middle - 1;
+    }
+  }
+
+  return -1;
+}
