f6de068cd2721354f7d4b66bf9f501c6ff1658e7
alirezanaseri548 <alirezanaseri369@gmail.com>
2026-07-29 13:53:59 +0330

    docs: connect examples in entry point

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-export {};
+import { linearSearch } from "./algorithms/linear-search";
+import { binarySearch } from "./algorithms/binary-search";
+import { bubbleSort } from "./algorithms/bubble-sort";
+
+console.log(linearSearch([5, 8, 2, 9], 2));
+console.log(binarySearch([1, 3, 5, 7, 9], 7));
+console.log(bubbleSort([9, 4, 1, 7, 3]));
