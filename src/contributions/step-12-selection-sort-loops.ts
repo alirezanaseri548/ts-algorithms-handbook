4719eff2782743d0cd28b8121c7c5102e5866185
Alireza Naseri <155387132+alirezanaseri548@users.noreply.github.com>
2026-07-30 13:26:18 +0330

    feat(contributions): implement selection sort loops - step 12

---
+++ src/contributions/step-12-selection-sort-loops.ts
@@ -0,0 +1,13 @@
+﻿export function selectionSortLoops(array: number[]): number[] {
+  for (let i = 0; i < array.length - 1; i++) {
+    let minIndex = i;
+
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j]! < array[minIndex]!) {
+        minIndex = j;
+      }
+    }
+  }
+
+  return array;
+}
