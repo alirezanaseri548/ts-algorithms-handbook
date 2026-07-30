6442377a796094e6c33c756f2dfe96711cfb2b2e
Alireza Naseri <155387132+alirezanaseri548@users.noreply.github.com>
2026-07-30 13:26:21 +0330

    feat(contributions): finalize selection sort - step 13

---
+++ src/contributions/step-13-selection-sort-complete.ts
@@ -0,0 +1,19 @@
+﻿export function selectionSort(array: number[]): number[] {
+  for (let i = 0; i < array.length - 1; i++) {
+    let minIndex = i;
+
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j]! < array[minIndex]!) {
+        minIndex = j;
+      }
+    }
+
+    if (minIndex !== i) {
+      const temp = array[i]!;
+      array[i] = array[minIndex]!;
+      array[minIndex] = temp;
+    }
+  }
+
+  return array;
+}
