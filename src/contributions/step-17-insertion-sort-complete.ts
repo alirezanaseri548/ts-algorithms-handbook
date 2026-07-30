920903a9c122a0299eb485213125ce227fc2802a
Alireza Naseri <155387132+alirezanaseri548@users.noreply.github.com>
2026-07-30 13:26:35 +0330

    feat(contributions): complete insertion sort implementation - step 17

---
+++ src/contributions/step-17-insertion-sort-complete.ts
@@ -0,0 +1,15 @@
+﻿export function insertionSort(array: number[]): number[] {
+  for (let i = 1; i < array.length; i++) {
+    const tempValue = array[i]!;
+    let position = i;
+
+    while (position > 0 && array[position - 1]! > tempValue) {
+      array[position] = array[position - 1]!;
+      position--;
+    }
+
+    array[position] = tempValue;
+  }
+
+  return array;
+}
