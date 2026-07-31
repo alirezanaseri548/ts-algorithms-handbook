ec29458830948d816f22da4bc5d9953b0825957b
Alireza Naseri <155387132+alirezanaseri548@users.noreply.github.com>
2026-07-31 16:01:07 +0330

    feat(step-16): add two-letter word builder

---
+++ src/algorithms/word-builder-two.ts
@@ -0,0 +1,13 @@
+export function wordBuilderTwo(characters: string[]): string[] {
+  const collection: string[] = [];
+
+  for (let i = 0; i < characters.length; i++) {
+    for (let j = 0; j < characters.length; j++) {
+      if (i !== j) {
+        collection.push(characters[i] + characters[j]);
+      }
+    }
+  }
+
+  return collection;
+}
---
+++ src/contributions/step-16-word-builder-two.ts
@@ -0,0 +1,1 @@
+export { wordBuilderTwo } from "../algorithms/word-builder-two";
