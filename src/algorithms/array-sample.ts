720dd59a2c5791802369e7487331d69df78de817
Alireza Naseri <155387132+alirezanaseri548@users.noreply.github.com>
2026-07-31 16:49:37 +0330

    fix(step-5): resolve strict typing in array-sample

--- src/algorithms/array-sample.ts
+++ src/algorithms/array-sample.ts
@@ -1,11 +1,11 @@
 export function arraySample<T>(items: T[]): [T, T, T] | null {
-  if (items.length === 0) {
+  if (items.length < 3) {
     return null;
   }
 
-  const first = items[0];
-  const middle = items[Math.floor(items.length / 2)];
-  const last = items[items.length - 1];
+  const first = items[0]!;
+  const middle = items[Math.floor(items.length / 2)]!;
+  const last = items[items.length - 1]!;
 
   return [first, middle, last];
 }
