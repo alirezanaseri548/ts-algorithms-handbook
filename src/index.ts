8fc7270b709660965bf74bf08f01eb76be50cd93
Alireza Naseri <155387132+alirezanaseri548@users.noreply.github.com>
2026-07-31 16:49:36 +0330

    fix(step-1): restore src/index.ts imports and usage

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import { linearSearch } from "./algorithms/linear-search";
-import { binarySearch } from "./algorithms/binary-search";
-import { bubbleSort } from "./algorithms/bubble-sort";
-import { twoSumHasPair } from "./algorithms/two-sum";
-import { containsX } from "./algorithms/contains-x";
-import { averageOfEvenNumbers } from "./algorithms/average-of-even-numbers";
-import { wordBuilderTwo } from "./algorithms/word-builder-two";
-import { wordBuilderThree } from "./algorithms/word-builder-three";
-import { arraySample } from "./algorithms/array-sample";
-import { intersectionWithBreak } from "./algorithms/array-intersection";
-import { printBigOSimplificationExamples } from "./examples/big-o-simplification";
+import { linearSearch } from "./algorithms/linear-search.js";
+import { binarySearch } from "./algorithms/binary-search.js";
+import { bubbleSort } from "./algorithms/bubble-sort.js";
+import { twoSumHasPair } from "./algorithms/two-sum.js";
+import { containsX } from "./algorithms/contains-x.js";
+import { averageOfEvenNumbers } from "./algorithms/average-of-even-numbers.js";
+import { wordBuilderTwo } from "./algorithms/word-builder-two.js";
+import { wordBuilderThree } from "./algorithms/word-builder-three.js";
+import { arraySample } from "./algorithms/array-sample.js";
+import { intersectionWithBreak } from "./algorithms/array-intersection.js";
+import { printBigOSimplificationExamples } from "./examples/big-o-simplification.js";
 
 console.log("Linear Search:", linearSearch([3, 7, 9, 12], 9));
 console.log("Binary Search:", binarySearch([1, 3, 5, 7, 9], 7));
