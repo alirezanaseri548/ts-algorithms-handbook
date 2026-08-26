b8bffb63dec729ba38aa2b08f6fed9ebd3ddac66
Alireza Naseri <alirezanaseri369@gmail.com>
2026-08-26 10:24:31 +0330

    fix: repair singly linked list API

--- src/data-structures/index.ts
+++ src/data-structures/index.ts
@@ -1,5 +1,3 @@
-﻿import "./compatibility.js";
-
 export { ListNode, SinglyLinkedList } from "./singly-linked-list.js";
 export { DoublyNode, DoublyLinkedList } from "./doubly-linked-list.js";
 export { LinkedListQueue } from "./linked-list-queue.js";
